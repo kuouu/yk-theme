82c52afbeed24b2b888b009dddea1dbece560df1
kuouu <erickuo5124@gmail.com>
2022-12-27 13:44:52 +0800

    style: add padding to inserted html tag

--- src/Router.js
+++ src/Router.js
@@ -9,7 +9,8 @@ import Home from "./pages/Home";
 
 const Router = () => {
   const [wpRouter, setWpRouter] = useState([]);
-  const ErrorElement = () =>
+
+  const Loading = () =>
     <div className="h-screen flex flex-col justify-center items-center gap-4">
       <svg class="animate-spin h-5 w-5 mr-3 text-slate-900" viewBox="0 0 24 24">
         <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z" />
@@ -19,7 +20,7 @@ const Router = () => {
   const customRouter = [
     { path: "/", element: <Home />, exact: true },
     { path: "/about", element: <div>About Pageeee</div>, },
-    { path: "*", element: <ErrorElement /> }
+    { path: "*", element: <Loading /> }
   ]
 
   useEffect(() => {
@@ -29,14 +30,14 @@ const Router = () => {
         `${baseUrl}?rest_route=/wp/v2/pages`
       );
       let { data } = await res;
-
       setWpRouter(data.map(page => {
+        const element = <div
+          className="py-4 px-8"
+          dangerouslySetInnerHTML={{ __html: page.content?.rendered }}
+        />
         return {
           path: page.slug,
-          element: <div
-            className="h-screen flex justify-center items-center"
-            dangerouslySetInnerHTML={{ __html: page.content?.rendered }}
-          />,
+          element,
         }
       }));
     };
