c8643cbfcc03b2e53f24ce16a6fd96dba15c9916
kuouu <erickuo5124@gmail.com>
2023-01-15 13:52:12 +0800

    feat(index.php): change index file routing

--- index.php
+++ index.php
@@ -4,16 +4,13 @@ get_header();
 if (is_product_category()) {
   echo '<div id="category-banner"></div>';
 } 
-if (have_posts()) {
-  while (have_posts()) {
-    the_post();
-    ?>
-      <div class="prose max-w-full">
-        <h3><?php the_title(); ?></h3>
-        <?php the_content(); ?>
-      </div>
-    <?php 
-  }
+if (is_page()) {
+  ?>
+    <div class="prose max-w-full">
+      <h3><?php the_title(); ?></h3>
+      <?php the_content(); ?>
+    </div>
+  <?php 
 } else {
   echo '<div id="root"></div>';
 }
