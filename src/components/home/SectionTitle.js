57e720c951349c5a5b93f53d180c69425e99aeaa
kuouu <erickuo5124@gmail.com>
2023-01-16 15:56:06 +0800

    style: change logo and section title

--- src/components/home/SectionTitle.js
+++ src/components/home/SectionTitle.js
@@ -1,32 +1,11 @@
-import { useRef, useEffect, useState } from 'react';
-
 const SectionTitle = ({ text }) => {
-  const [width, setWidth] = useState(0);
-  const sectionTitleRef = useRef(null);
-  useEffect(() => {
-    setWidth(sectionTitleRef.current.offsetWidth + 10);
-  }, [sectionTitleRef]);
   return (
-    <div className='my-5 h-6'>
-      <div
-        className="w-max bg-[#062398]"
-        style={{ ...trapezium, height: '45px', width: `${width}px` }}
-      />
-      <div
-        className="w-max bg-[#7a91ef] relative top-[-55px]"
-        style={trapezium}
-        ref={sectionTitleRef}
-      >
-        <h2 className="text-3xl font-bold text-slate-100 mx-10 mr-14">
-          {text}
-        </h2>
-      </div>
+    <div className='mx-10 my-5 border-b border-[#2f2f2f]'>
+      <h2 className="text-3xl font-bold mb-2">
+        {text}
+      </h2>
     </div>
   );
 }
-
-const trapezium = {
-  clipPath: 'polygon(0 0, 100% 0, 90% 100%, 0% 100%)'
-};
 
 export default SectionTitle;
