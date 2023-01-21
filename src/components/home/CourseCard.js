5ee38f2d00093c021408e200640f127dd97134d1
kuouu <erickuo5124@gmail.com>
2023-01-21 22:54:31 +0800

    feat(Home): update section content

--- src/components/home/CourseCard.js
+++ src/components/home/CourseCard.js
@@ -1,19 +1,52 @@
-const CourseCard = ({ course }) => {
+const CourseCard = ({ course, isPromotion }) => {
+  const handleClick = (link) => {
+    window.location.href = link
+  }
   return (
     <div className="flex flex-col justify-center items-center">
-      <div className="w-64 bg-zinc-700 rounded-lg shadow-lg p-4">
-        <img
-          src={'https://picsum.photos/200/300'}
-          alt={''}
-          className="w-full h-32 object-cover rounded-lg"
-        />
-        <div className="mt-4">
-          <h3 className="text-xl font-bold text-slate-900">{'Course Name'}</h3>
-          <p className="text-sm text-slate-700">{'Course Description'}</p>
+      <div className="w-72 bg-zinc-700 rounded-lg shadow-lg p-4">
+        <div className="cursor-pointer" onClick={() => handleClick(course.link)}>
+          <img
+            src={course.img}
+            className="w-full h-32 object-cover rounded-lg"
+          />
+          <div className="mt-4">
+            <h3 className="text-xl font-bold text-slate-900">{course.name}</h3>
+          </div>
+          <div className="flex gap-2 items-center mt-4">
+            {isPromotion && (<>
+              <div className="text-xs bg-yellow-500 py-1 px-2 rounded">限時特價中</div>
+              <div className="flex items-center gap-2">
+                <span className="text-sm text-slate-900 line-through">NT${course.price}</span>
+              </div>
+            </>)}
+            <div className="flex items-center gap-2">
+              <span className={`text-md font-bold ${isPromotion ? 'text-red-700' : 'text-zinc-50'}`}>
+                NT${course.promotion}
+              </span>
+            </div>
+          </div>
         </div>
+        <button
+          style={buttonStyle}
+          className="hover:bg-white hover:text-[#00788c]"
+          onClick={() => handleClick(course.cart)}
+        >
+          加入購物車
+        </button>
       </div>
     </div>
   );
+}
+
+const buttonStyle = {
+  width: '100%',
+  fontWeight: 'bold',
+  textAlign: 'center',
+  padding: '0.5rem 1rem',
+  borderRadius: '0.5rem',
+  marginTop: '1rem',
+  border: '1px solid #29d7ff',
 }
 
 export default CourseCard;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,18 +7,44 @@ import bg1 from "../assets/homepage-bg1.png";
 import bg2 from "../assets/homepage-bg2.png";
 
 const Home = () => {
+  const courses = [
+    {
+      name: "土壤力學",
+      img: "https://yourknowledge.online/wp-content/uploads/2022/07/ch1_220702_0.jpg",
+      price: 8000,
+      promotion: 4500,
+      link: "https://yourknowledge.online/courses/soil-mechanics/",
+      cart: "https://yourknowledge.online/courses/?add-to-cart=25397"
+    },
+    {
+      name: "基礎工程",
+      img: "https://yourknowledge.online/wp-content/uploads/2022/09/hqdefault.jpg",
+      price: 8000,
+      promotion: 6000,
+      link: "https://yourknowledge.online/courses/foundation-engineering/",
+      cart: "https://yourknowledge.online/courses/?add-to-cart=25800"
+    },
+  ]
   return (
     <div>
       <Jumbotron />
       <div className="py-14 bg-right bg-contain bg-no-repeat" style={{backgroundImage: `url(${bg1})`}}>
         <SectionTitle text="課程推薦"/>
-        <div className="pt-14 flex justify-center gap-8">
-          <CourseCard />
-          <CourseCard />
-          <CourseCard />
+        <div className="pt-14 flex justify-around">
+          {courses.map((course, index) => (
+            <CourseCard key={index} course={course} isPromotion={true} />
+          ))}
         </div>
       </div>
       <div className="py-14 bg-left bg-contain bg-no-repeat" style={{backgroundImage: `url(${bg2})`}}>
+        <SectionTitle text="熱門課程"/>
+        <div className="pt-14 flex justify-around">
+          {courses.map((course, index) => (
+            <CourseCard key={index} course={course} isPromotion={false} />
+          ))}
+        </div>
+      </div>
+      <div className="py-14 bg-right bg-contain bg-no-repeat" >
         <SectionTitle text="專業師資" />
         <div className="pt-14 flex justify-center gap-8">
           <TeacherProfile />
