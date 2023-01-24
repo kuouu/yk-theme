eaa8b8d716e11480cc10bc172d8336b1207c9afb
kuouu <erickuo5124@gmail.com>
2023-01-25 03:22:05 +0800

    feat(Home-Jumbotron): add carousel

--- src/components/home/Jumbotron.js
+++ src/components/home/Jumbotron.js
@@ -1,18 +1,18 @@
 import { useState, useEffect } from 'react'
 
+import carousel1 from '../../assets/carousel/carousel_1.png'
+import carousel2 from '../../assets/carousel/carousel_2.png'
+import carousel3 from '../../assets/carousel/carousel_3.png'
+
 const Jumbotron = () => {
   const [imgIdx, setImgIdx] = useState(0)
-  const imageSrc = [
-    'https://picsum.photos/1920/1080',
-    'https://picsum.photos/1920/1080',
-    'https://picsum.photos/1920/1080',
-  ]
+  const imageSrc = [carousel1, carousel2, carousel3]
   const nextImg = () => setImgIdx((imgIdx + 1) % imageSrc.length)
   useEffect(() => {
     const timer = setInterval(nextImg, 5000)
     return () => clearInterval(timer)
   })
-  
+
   return (
     <div className="h-screen relative">
       <div className='flex gap-2 absolute bottom-8 w-full justify-center z-10'>
@@ -22,7 +22,7 @@ const Jumbotron = () => {
       </div>
       <img
         src={imageSrc[imgIdx]}
-        className="h-full w-full opacity-50"
+        className="h-full w-full opacity-50 object-cover object-center"
       />
     </div>
   )
