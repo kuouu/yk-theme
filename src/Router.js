da29c0b6f3d03dd2c9df0504561713b7eb3391cb
Kuo Yu-Tsung <erickuo5124@gmail.com>
2023-08-01 23:32:50 +0800

    feat(Header): enable crowdfunding page

--- src/Router.js
+++ src/Router.js
@@ -9,10 +9,10 @@ const Router = () => {
 
   const customRouter = [
     { path: "/", element: <Home />, exact: true },
-    {
-      path: "/crowdfunding",
-      element: <div><img src={'https://yourknowledge.online/wp-content/uploads/2023/01/comingsoon.png'} /></div>
-    }
+    // {
+    //   path: "/crowdfunding",
+    //   element: <div><img src={'https://yourknowledge.online/wp-content/uploads/2023/01/comingsoon.png'} /></div>
+    // }
   ]
 
   return <RouterProvider router={createBrowserRouter(customRouter)} />
