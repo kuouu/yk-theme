8a8203423d25cafb884024e452217610206f0ffe
Kuo Yu-Tsung <erickuo5124@gmail.com>
2023-08-19 02:31:30 +0800

    add chef-ricky to subitem

--- navbar.php
+++ navbar.php
@@ -9,13 +9,14 @@
     <?php
         $links = array(
         array('name' => '講義專區', 'link' => home_url('/tag/handouts')),
-        array('name' => '預購專區', 'link' => home_url('/product/chef-ricky')),
+        array('name' => '預購專區', 'link' => home_url('/courses/chef-ricky')),
         array('name' => '購物車', 'link' => home_url('/cart')),
         array('name' => '我的帳號', 'link' => home_url('/dashboard'))
         );
         $courseLinks = array(
             array('name' => '土壤力學', 'link' => home_url('/courses/soil-mechanics/')),
-            array('name' => '基礎工程', 'link' => home_url('/courses/foundation-engineering/'))
+            array('name' => '基礎工程', 'link' => home_url('/courses/foundation-engineering/')),
+            array('name' => '西班牙料理', 'link' => home_url('/courses/chef-ricky/'))
         );
     ?>
     
@@ -25,9 +26,9 @@
         <!-- course submenu -->
         <div class="ml-4 relative group">
             <a href="#" class="text-white hover:text-gray-300 cursor-pointer" onclick="toggleDesktopSubmenu(event)">精選課程</a>
-            <div id="desktopSubmenu" class="z-20 absolute left-0 mt-4 w-24 bg-zinc-800 text-white hidden shadow-lg rounded">
+            <div id="desktopSubmenu" class="z-20 absolute left-0 mt-4 w-28 bg-zinc-800 text-white hidden shadow-lg rounded">
                 <?php foreach ($courseLinks as $course) : ?>
-                <a href="<?php echo $course['link']; ?>" class="block px-4 py-2 hover:bg-blue-100 hover:text-blue-400">
+                <a href="<?php echo $course['link']; ?>" class="text-center block px-4 py-2 hover:bg-blue-100 hover:text-blue-400">
                     <?php echo $course['name']; ?>
                 </a>
                 <?php endforeach; ?>
