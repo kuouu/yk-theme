2a85828d9fbb0d5aa1e2c5a2c8905e57d89f4d46
Kuo Yu-Tsung <erickuo5124@gmail.com>
2023-08-26 03:29:14 -0700

    feat(trending-course): apply href to all component

--- components/trending-course.php
+++ components/trending-course.php
@@ -2,22 +2,21 @@
 $course = isset($args['course']) ? $args['course'] : array();
 ?>
 
-<div class="w-80 bg-zinc-900 shadow-lg overflow-hidden shadow-md hover:shadow-2xl transition-shadow duration-300">
-    <div>
-        <a href="<?php echo esc_url($course['link']); ?>">
-            <img alt="<?php echo esc_attr($course['name']); ?>" src="<?php echo esc_url($course['image']); ?>"
-                class="w-full h-48 object-cover">
-        </a>
-    </div>
+<div 
+    class="w-80 bg-zinc-900 shadow-lg overflow-hidden shadow-md hover:shadow-2xl transition-shadow duration-300 cursor-pointer"
+    onclick="location.href='<?php echo esc_url($course['link']); ?>'"
+>
+    <img alt="<?php echo esc_attr($course['name']); ?>" src="<?php echo esc_url($course['image']); ?>"
+        class="w-full h-48 object-cover">
     <div class="p-4">
         <div class="bg-gray-700 text-white px-2 py-1 rounded-full inline-block mb-2">
             立即上課
         </div>
-        <a href="<?php echo esc_url($course['link']); ?>" class="block mb-2">
+        <div class="mb-2">
             <h3 class="text-xl text-white truncate">
                 <?php echo esc_html($course['name']); ?>
             </h3>
-        </a>
+        </div>
         <div class="text-sm text-gray-300 mb-2 truncate">
             by <?php echo esc_html($course['teacher']); ?>
         </div>
